e74e9a874e6b8d531ab3ac32f2d565f2d331fab6
Juan José Fernández <juanjofer1999@gmail.com>
2020-05-13 21:57:30 -0400

    teacher.ts comentado

--- src/entity/Teacher.ts
+++ src/entity/Teacher.ts
@@ -1,34 +1,34 @@
-import{Entity,Column,PrimaryGeneratedColumn} from 'typeorm';
-import bcrypt from 'bcrypt';
-@Entity()
-export class Teacher{
-    @PrimaryGeneratedColumn("increment")
-    id_docente:bigint;
-    
-    @Column()
-    nombre_docente:string;
-    
-    @Column()
-    ap_pat_docente:string; 
+// import{Entity,Column,PrimaryGeneratedColumn} from 'typeorm';
+// import bcrypt from 'bcrypt';
+// @Entity()
+// export class Teacher{
+//     @PrimaryGeneratedColumn("increment")
+//     id_docente:bigint;
 
-    @Column()
-    ap_mat_docente:string;
+//     @Column()
+//     nombre_docente:string;
 
-    @Column()
-    correo_docente:string;
+//     @Column()
+//     ap_pat_docente:string;
 
-    @Column()
-    contrasenia_docente:string;
+//     @Column()
+//     ap_mat_docente:string;
 
-    @Column()
-    estado_docente:number;
+//     @Column()
+//     correo_docente:string;
 
-    criptPassword():void{
-        const salt=bcrypt.genSaltSync(10);
-        this.contrasenia_docente=bcrypt.hashSync(this.contrasenia_docente,salt);
-    }
-    //!!Verificar si funciona cuando las funciones son asyncronas
-    checkPassword(password:string):boolean{
-        return bcrypt.compareSync(password,this.contrasenia_docente);
-    }
-}
+//     @Column()
+//     contrasenia_docente:string;
+
+//     @Column()
+//     estado_docente:number;
+
+//     criptPassword():void{
+//         const salt=bcrypt.genSaltSync(10);
+//         this.contrasenia_docente=bcrypt.hashSync(this.contrasenia_docente,salt);
+//     }
+//     //!!Verificar si funciona cuando las funciones son asyncronas
+//     checkPassword(password:string):boolean{
+//         return bcrypt.compareSync(password,this.contrasenia_docente);
+//     }
+// }
