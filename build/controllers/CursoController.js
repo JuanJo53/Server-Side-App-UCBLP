52d846d38b3be5d612547f48a2067ee362afb3c3
pjalvin <pomaalvin@gmail.com>
2020-05-18 21:28:34 -0400

    Class-room endpoint

--- build/controllers/CursoController.js
+++ build/controllers/CursoController.js
@@ -18,6 +18,7 @@ class CursoController {
         return __awaiter(this, void 0, void 0, function* () {
             const id = req.docenteId;
             console.log("ID: " + id);
+            //Regresa la informacion basica de un curso
             const query = `select curso.id_curso  ,curso.nombre_curso, semestre.semestre, count(curso_alumno.id_alumno) as estudiantes  
         from curso inner join semestre on 
         curso.id_semestre = semestre.id_semestre 
@@ -29,28 +30,33 @@ class CursoController {
         on curso.id_docente=docente.id_docente 
         where curso.estado_curso=true and docente.id_docente= ?
         group by curso.id_curso`;
-            const query2 = `select curso.id_curso as 'idCurso',dia_semana.dia_semana as 'diaSemana', curso_dia.hora_inicio as 'horaInicio', curso_dia.hora_conclusion as 'horaConclusion'
+            //Regresa la informacion de los dias y horarios del curso
+            const query3 = `select dia_semana.dia_semana as 'diaSemana', curso_dia.hora_inicio as 'horaInicio', curso_dia.hora_conclusion as 'horaConclusion'
         from curso_dia inner join dia_semana on
         curso_dia.id_dia_semana= dia_semana.id_dia_semana
         inner join curso on 
         curso.id_curso = curso_dia.id_curso
         inner join docente on
         docente.id_docente = curso.id_docente
-        where docente.id_docente= ? and curso.estado_curso=true
-        group by curso.id_curso, dia_semana.id_dia_semana, curso_dia.id_curso_dia`;
+        where curso.id_curso=?`;
             //Arreglo que almacena los resultados de las consultas
             let resultData = [];
             yield Database_1.default.query(query, [id], function (err, result, fields) {
                 if (err)
                     throw err;
-                //res.json(result);
-                resultData.push(result);
-                Database_1.default.query(query2, [id], function (err2, result2, fields2) {
-                    if (err2)
-                        throw err2;
-                    resultData.push(result2);
-                    res.json(resultData[1]);
-                });
+                //retorna la informacion basica del curso agregando los dias y horarios de cada uno
+                var c = 1;
+                for (let i in result) {
+                    Database_1.default.query(query3, [result[i].id_curso], function (err2, result3, fields2) {
+                        if (err2)
+                            throw err2;
+                        result[i].dias = result3;
+                        if (c == result.length) {
+                            res.json(result);
+                        }
+                        c++;
+                    });
+                }
             });
         });
     }
