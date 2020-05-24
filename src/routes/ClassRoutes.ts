d6b4c2610750e3dda4a532aa2ba0e0a2e7275a20
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-05-24 17:47:31 -0400

    Get student by email V2

--- src/routes/ClassRoutes.ts
+++ src/routes/ClassRoutes.ts
@@ -14,7 +14,8 @@ class ClassRoutes{
         //this.router.get('/teacher/class-room',TokenValidation,cursoController.obtenerCursosDocente);
         this.router.get('/teacher/my-class/students/:id',TokenValidation,classController.listaAlumnos);
         this.router.delete('/teacher/my-class/students/:id',TokenValidation,classController.bajaAlumnoCurso);
-        this.router.get('/teacher/my-class/students/profile',classController.obtenerAlumnoAltaCurso);
+        this.router.post('/teacher/my-class/students/profile',TokenValidation,classController.obtenerAlumnoAltaCurso);
+        this.router.post('/teacher/my-class/students',TokenValidation,classController.obtenerAlumnoAltaCurso);
         
     }
 }
