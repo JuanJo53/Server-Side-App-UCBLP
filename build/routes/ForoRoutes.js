a5873a21b92839e84be0bd5103f5eebc3783ae97
Juan José Fernández <juanjofer1999@gmail.com>
2020-06-18 10:45:44 -0400

    Cambios de JS automaticos

--- build/routes/ForoRoutes.js
+++ build/routes/ForoRoutes.js
@@ -4,18 +4,19 @@ const express_1 = require("express");
 const VerifyToken_1 = require("../libs/VerifyToken");
 const ForoController_1 = require("../controllers/ForoController");
 class ForoRoutes {
-	constructor() {
-		this.router = express_1.Router();
-		this.config();
-	}
-	//configurar respuesta routas
-	config() {
-		this.router.post("/teacher/forums", VerifyToken_1.TokenValidation, ForoController_1.foroController.crearForo);
-		this.router.get("/teacher/forums/:id", VerifyToken_1.TokenValidation, ForoController_1.foroController.listarForos);
-		this.router.put("/teacher/forums/", VerifyToken_1.TokenValidation, ForoController_1.foroController.modificarForos);
-		this.router.delete("/teacher/forums", VerifyToken_1.TokenValidation, ForoController_1.foroController.eliminarForo);
-		this.router.post("/teacher/forums/message", ForoController_1.foroController.agregarMensaje);
-	}
+    constructor() {
+        this.router = express_1.Router();
+        this.config();
+    }
+    ;
+    //configurar respuesta routas
+    config() {
+        this.router.post('/teacher/forums', VerifyToken_1.TokenValidation, ForoController_1.foroController.crearForo);
+        this.router.get('/teacher/forums/:id', VerifyToken_1.TokenValidation, ForoController_1.foroController.listarForos);
+        this.router.put('/teacher/forums/', VerifyToken_1.TokenValidation, ForoController_1.foroController.modificarForos);
+        this.router.delete('/teacher/forums', VerifyToken_1.TokenValidation, ForoController_1.foroController.eliminarForo);
+        this.router.post('/teacher/forums/message', ForoController_1.foroController.agregarMensaje);
+    }
 }
 const foroRoutes = new ForoRoutes();
 exports.default = foroRoutes.router;
