890c3a2a776be55a6664ffd160afe199c6903d53
pjalvin <pomaalvin@gmail.com>
2020-07-20 23:04:25 -0400

    revision practica arreglado

--- src/controllers/Alumno_Controllers/PracticaController.ts
+++ src/controllers/Alumno_Controllers/PracticaController.ts
@@ -7,6 +7,7 @@ import util from 'util'
 
 
 class PracticaController{
+    
     public async infoPractica(req:Request,res:Response){
         const {id} = req.params;
         const idEstudiante=req.estudianteId;
@@ -101,10 +102,17 @@ class PracticaController{
             const db=firebase.firestore()
             var datos=await db.collection('Preguntas').where(firebase.firestore.FieldPath.documentId(),"in",listaPreg).get();
             
-            for(let preg in row){
-                var preguntaaux=datos.docs[preg].data();
-                preguntaaux.respuestas=[];
-                row[preg].pregunta=preguntaaux;
+            for(let preg in row){ 
+                var docs=datos.docs;
+                for(let doc of docs){ 
+                    if(doc.id===row[preg].codigo_pregunta){   
+                                             
+                        var preguntaaux=doc.data();
+                        preguntaaux.respuestas=[];
+                        row[preg].pregunta=preguntaaux;
+                        break;
+                    }
+                }
             }
             res.status(200).json(row);
         }
@@ -134,19 +142,65 @@ class PracticaController{
         const id = req.body.id;
         const idEstudiante=req.estudianteId;
         const preguntasR=req.body.preguntas as any[];
-        const query =`SELECT practica_pregunta.puntuacion_practica_pregunta,pregunta.id_tipo_pregunta,pregunta.id_tipo_respuesta,practica_pregunta.id_pregunta_practica ,practica_pregunta.id_pregunta,pregunta.codigo_pregunta,
+        const query =`SELECT pregunta.id_pregunta,curso.id_curso,practica_pregunta.puntuacion_practica_pregunta,pregunta.id_tipo_pregunta,pregunta.id_tipo_respuesta,practica_pregunta.id_pregunta_practica ,practica_pregunta.id_pregunta,pregunta.codigo_pregunta,
         practica_pregunta.puntuacion_practica_pregunta 
         FROM practica INNER JOIN practica_pregunta ON
         practica.id_practica = practica_pregunta.id_practica
         INNER JOIN pregunta ON
         pregunta.id_pregunta = practica_pregunta.id_pregunta
-        WHERE practica_pregunta.estado_pregunta_practica   = true
+        INNER JOIN leccion ON
+        leccion.id_leccion = practica.id_leccion
+        INNER JOIN tema ON
+        tema.id_tema=leccion.id_tema
+        INNER JOIN curso ON
+        curso.id_curso = tema.id_curso
+        INNER JOIN curso_alumno ON
+        curso_alumno.id_curso=curso.id_curso
+        INNER JOIN alumno ON
+        curso_alumno.id_alumno = alumno.id_alumno
+        WHERE practica.estado_practica=true
+        AND leccion.estado_leccion = true
+        AND tema.estado_tema = true
+        AND curso.estado_curso = true
+        AND curso_alumno.estado_curso_alumno = true
+        AND practica_pregunta.estado_pregunta_practica   = true
         AND CAST(CONCAT(DATE(practica.inicio_fecha),' ',practica.inicio_hora-INTERVAL 4 HOUR) AS DATETIME)<=NOW()
         AND CAST(CONCAT(DATE(practica.fin_fecha),' ',practica.fin_hora-INTERVAL 4 HOUR) AS DATETIME)>=NOW()
         AND practica.id_practica=?`;
         const agregarNota =`
             INSERT INTO nota_practica(id_practica,id_alumno,nota_practica,estado_nota_practica,tx_id,tx_username,tx_host,tx_date)
             VALUES (?,?,?,true,1,'root','192.168.0.10',CURRENT_TIMESTAMP())`
+        const obtenerNotasPractica=`
+        SELECT AVG(nota_practica.nota_practica) as promedio
+        FROM nota_practica INNER JOIN practica ON
+        practica.id_practica = nota_practica.id_practica
+        INNER JOIN leccion ON
+        leccion.id_leccion = practica.id_leccion
+        INNER JOIN tema ON
+        tema.id_tema=leccion.id_tema
+        INNER JOIN curso ON
+        curso.id_curso = tema.id_curso
+        INNER JOIN curso_alumno ON
+        curso_alumno.id_curso=curso.id_curso
+        INNER JOIN alumno ON
+        curso_alumno.id_alumno = alumno.id_alumno
+        WHERE practica.estado_practica=true
+        AND leccion.estado_leccion = true
+        AND tema.estado_tema = true
+        AND curso.estado_curso = true
+        AND curso_alumno.estado_curso_alumno = true
+        AND alumno.id_alumno=?` 
+        const moduloPractica=`
+        SELECT modulo.id_modulo 
+        FROM modulo INNER JOIN curso ON
+        curso.id_curso= modulo.id_curso
+        WHERE curso.id_curso = ?
+        AND curso.estado_curso = true
+        AND modulo.nombre_modulo = 'Theme Practices'
+        AND modulo.estado_modulo =true `
+        const agregarNotaModulo =`
+            UPDATE nota_modulo set nota_modulo=?,estado_nota_modulo=true,tx_id=1,tx_username='root',tx_host='192.168.0.10',tx_date=CURRENT_TIMESTAMP()
+            WHERE id_alumno=? AND id_modulo=?`
     try{ 
         const result2:(arg1:string,arg2?:any[])=>Promise<unknown> = util.promisify(Db.query).bind(Db);
         var row =await result2(query,[id]) as any[];
@@ -154,6 +208,7 @@ class PracticaController{
             res.status(500).json({text:'No esta habilitado para ver esta practica'})
         }   
         else{            
+            console.log(row);
             if(row.length!=preguntasR.length){
                 res.status(500).json({text:'Error en los datos Enviados'})
                 
@@ -166,14 +221,26 @@ class PracticaController{
                 const db=firebase.firestore()
                 var datos=await db.collection('Preguntas').where(firebase.firestore.FieldPath.documentId(),"in",listaPreg).get();
                 var puntuacion=0;
-                for(let preg in row){
+                for(let preg in preguntasR){
+                    var docs=datos.docs;
+                        for(let doc of docs){ 
+                            if(doc.id===preguntasR[preg].codigoPregunta){   
+                                                    
+                                var preguntaaux=doc.data();
+                                if(practicaController.compararRespuestas(preguntaaux.respuestas,preguntasR[preg].respuesta)){
+                                    puntuacion+=row[preg].puntuacion_practica_pregunta;
+                                }
+                                break;
+                            }
+                        }
                     var preguntaaux=datos.docs[preg].data();
-                    if(practicaController.compararRespuestas(preguntaaux.respuestas,preguntasR[preg].respuesta)){
-                        puntuacion+=row[preg].puntuacion_practica_pregunta;
-                    }
-                    row[preg].pregunta=preguntaaux;
+                    
                 }
                 await result2(agregarNota,[id,idEstudiante,puntuacion,id,idEstudiante,]) as any[];
+                var promedioPracticas=await result2(obtenerNotasPractica,[idEstudiante]) as any[];
+                var idModulo=await result2(moduloPractica,[row[0].id_curso]) as any[];
+                await result2(agregarNotaModulo,[promedioPracticas[0].promedio,idEstudiante,idModulo[0].id_modulo]) as any[];
+                console.log(idModulo);
                 res.status(200).json("Se califico el examen correctamente");
                 
             }
